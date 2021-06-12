e0ee0fe4f12468cd7bcd04fb5541a4fed1bb0689
Lizon <r.mahotkina@gmail.com>
2021-06-12 21:54:16 +0300

    апи без heroku

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ApplicationRecord
-	has_many :todos
+	  has_many :todos, dependent: :destroy
+
+  validates :projectname,
+            presence: true,
+            uniqueness: true
 	scope :with_todos, -> { Project.includes(:todos).where.not(todos: {id: nil}).order(:updated_at) }
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,23 @@
 class Todo < ApplicationRecord
-  belongs_to :project
+ belongs_to :project
+
+  validates :todoname, :project,
+            presence: true
+			# checked : false
+
+  validate :ensure_not_checked, on: :create
+@opened=false;
+@checked=false;
+ def is_opened?
+  return @opened
+end
+
+def check
+	@checked=true;
+end
+
+
+  def ensure_not_checked
+    errors.add(:todo, "can't be already completed!") if checked?
+  end
 end
