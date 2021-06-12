9e9e93c597eac51213aceb06b243148710646935
Lizon <r.mahotkina@gmail.com>
2021-06-12 16:15:47 +0300

    GET /projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+seed_file = File.join(Rails.root, 'db', 'seeds.yml')
+config = YAML.load_file(seed_file)
+conf = HashWithIndifferentAccess.new(config)
+    conf[:projects].each do |project|
+        @project = Project.create({title: project[:title]})
+        puts project[:title]
+        project[:todos].each do |todo|
+            @project.todos.create ({text: todo[:text], isCompleted: todo[:isCompleted]})
+            puts todo[:text]
+        end
+        @todos = @project.todos.all
+        puts "\n"
+      end
